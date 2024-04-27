8c45cb408f5564d807deb78eac24749d47c31fbb
Kaushik Mishra <kaushik.ultimate96@gmail.com>
2024-04-27 19:32:44 +0530

    resolved observation space, implmented observation render

--- Environment/custom_env.py
+++ Environment/custom_env.py
@@ -19,7 +19,12 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 PINK = (255,192,203)
 BLUE = (0,0,255)
+PEACH = (255, 220, 212)
+GRAPE = (161, 255, 138)
+GRAY = (200, 200, 200)
+DARKGREEN = ( 52, 141, 30, 20 )
 AGENT_COLORS = [RED, BLUE, PINK]
+CELL_COLORS = [PEACH, DARKGREEN]
 
 ac_config = open('Environment/ac_config.json')
 ac_variables = json.load(ac_config)
@@ -331,7 +336,7 @@ class CustomEnvironment(ParallelEnv):
         self.timestep += 1
 
         if(self.render_mode == "human"):
-            self.render()
+            self.render(observations)
 
         return observations, rewards, terminations, truncations, infos
     
@@ -395,15 +400,18 @@ class CustomEnvironment(ParallelEnv):
         #create a box centered on the agent of unit length. this is the bounds of the agent
         #based on vision(min = 3) based on this, the agent can see upto 3 boxes around itself(square fasion mai)
         #set the observation matrix of the agent based on the information from the boxes
-        dist_x = agent.pos_x - entity.pos_x
-        dist_y = agent.pos_y - entity.pos_y
+        # dist_x = agent.pos_x - entity.pos_x
+        # dist_y = agent.pos_y - entity.pos_y
+
+        dist = math.sqrt((agent.pos_x - entity.pos_x)**2 + (agent.pos_y - entity.pos_y)**2)
 
         # #entity not within the agent's vision
         # if(abs(dist_x) > agent.vision_size+0.5 or abs(dist_y) > agent.vision_size+0.5):
         #     return "NOT_VISIBLE"
 
         #entity with the pellet's vision and hitting the agent
-        if(abs(dist_x) <= 1 and abs(dist_y) <= 1):
+        # if(abs(dist_x) <= 1 and abs(dist_y) <= 1):
+        if(dist <= 1):
             return True
         
         return False
@@ -437,29 +445,29 @@ class CustomEnvironment(ParallelEnv):
                 y = math.floor(agent.pos_y + j)
                 
                 #initiate nothing is of interest in these
-                box[0,self.max_vision_size+i,self.max_vision_size+j] = 0
-                box[1,self.max_vision_size+i,self.max_vision_size+j] = 0
-                box[2,self.max_vision_size+i,self.max_vision_size+j] = 0
+                box[0,self.max_vision_size+j,self.max_vision_size+i] = 0
+                box[1,self.max_vision_size+j,self.max_vision_size+i] = 0
+                box[2,self.max_vision_size+j,self.max_vision_size+i] = 0
                 
                 for tup in agent_pos.items():
                     id,pos = tup
                     # if inside the vision size, then set the strength of the other agent as the value 
                     if [x,y] == pos:
-                        box[0,self.max_vision_size+i,self.max_vision_size+j] = self.agents_objects[id].strength
+                        box[0,self.max_vision_size+j,self.max_vision_size+i] = self.agents_objects[id].strength
 
                 #inside the vision size and grid and is a pellet 
                 if [x,y] in pellet_pos:
                     dist = math.sqrt((x - agent.pos_x)**2 + (y - agent.pos_y)**2)
                     min_pellet_dist = min(min_pellet_dist, dist)
-                    box[1,self.max_vision_size+i,self.max_vision_size+j] = 1
+                    box[1,self.max_vision_size+j,self.max_vision_size+i] = 1
 
                 #inside the vision size but outside the grid
                 if  x < 0 or x >= self.grid_size_x or y < 0 or y >= self.grid_size_y:
-                    box[2,self.max_vision_size+i,self.max_vision_size+j] = 1
+                    box[2,self.max_vision_size+j,self.max_vision_size+i] = 1
 
         return box, min_pellet_dist
 
-    def render(self):
+    def render(self, observations):
         pygame.event.get()
         self.screen.fill(WHITE)
         if len(self.agents_objects) > 0:
@@ -476,6 +484,38 @@ class CustomEnvironment(ParallelEnv):
                 screen_pos = [agent.pos_x * (self.screen_width / self.grid_size_x), agent.pos_y * (self.screen_height / self.grid_size_y)]
                 pygame.draw.circle(self.screen, AGENT_COLORS[id], screen_pos, 5)
 
+            # Draw grid around the circles for their observation space
+            cell_dims = 1
+            grid_size = 2*self.max_vision_size+1
+            for id,agent in self.agents_objects.items():
+                top_left_x = agent.pos_x - (grid_size * cell_dims) / 2
+                top_left_y = agent.pos_y - (grid_size * cell_dims) / 2
+                screen_top_left_x = top_left_x * (self.screen_width / self.grid_size_x)
+                screen_top_left_y = top_left_y * (self.screen_height / self.grid_size_y)
+                scaled_grid_size = grid_size * (self.screen_height / self.grid_size_y)
+                # Draw horizontal lines
+                for row in range(grid_size):
+                    y = top_left_y + row * cell_dims
+                    screen_y = y * (self.screen_height / self.grid_size_y)
+                    pygame.draw.line(self.screen, GRAY, (screen_top_left_x, screen_y), (screen_top_left_x + scaled_grid_size * cell_dims, screen_y))
+                # Draw vertical lines
+                for col in range(grid_size):
+                    x = top_left_x + col * cell_dims
+                    screen_x = x * (self.screen_width / self.grid_size_x)
+                    pygame.draw.line(self.screen, GRAY, (screen_x, screen_top_left_y), (screen_x, screen_top_left_y + scaled_grid_size * cell_dims))
+
+                for row in range(grid_size):
+                    for col in range(grid_size):
+                        cell_value = int(observations[id][1][row][col])
+                        x = top_left_x + col * cell_dims
+                        y = top_left_y + row * cell_dims
+                        if(cell_value == 0):
+                            continue
+                        scaled_cell_dims = cell_dims * (self.screen_height / self.grid_size_y)
+                        screen_x = x * (self.screen_width / self.grid_size_x)
+                        screen_y = y * (self.screen_height / self.grid_size_y)
+                        pygame.draw.rect(self.screen, CELL_COLORS[cell_value], (screen_x, screen_y, scaled_cell_dims, scaled_cell_dims))
+
             # Draw the food circles (relative to coordinate space position)
             for pos in pellet_pos:
                 screen_pos = [pos[0] * (self.screen_width / self.grid_size_x), pos[1] * (self.screen_height / self.grid_size_y)]
@@ -487,7 +527,7 @@ class CustomEnvironment(ParallelEnv):
         self.screen.blit(text_surface, (0, 0))
         pygame.display.flip()
 
-        # sleep(0.01)
+        sleep(1)
 
     # Observation space should be defined here.
     @functools.lru_cache(maxsize=None)
